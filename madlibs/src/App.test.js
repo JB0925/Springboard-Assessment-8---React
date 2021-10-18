2faf3201f55107a5b3811a6ed5bd853d9c295385
Jesse Brink <jbrink0925@gmail.com>
2021-10-17 23:52:37 -0400

    add smoke and snapshot tests for Madlibs app.

--- madlibs/src/App.test.js
+++ madlibs/src/App.test.js
@@ -1,8 +1,47 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import MadLib from "./MadLib";
+import MadLibForm from "./MadLibForm";
+import MadLibArea from "./MadLibArea";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Smoke tests for the Madlibs app - do the components render as they should?", () => {
+  test('renders the App component', () => {
+    render(<App />);
+  });
+
+  test('renders the MadLib component', () => {
+    render(<MadLib />);
+  });
+
+  test('renders the MadLibForm component', () => {
+    render(<MadLibForm />);
+  });
+
+  test('renders the MadLibArea component', () => {
+    render(<MadLibArea />);
+  });
 });
+
+describe('Snapshot tests - does each component match the snapshot?', () => {
+  test('The App component matches the snapshot', () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('The MadLib component matches the snapshot', () => {
+    const { asFragment } = render(<MadLib />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('The MadLibForm component matches the snapshot', () => {
+    const { asFragment } = render(<MadLibForm />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('The MadLibArea component matches the snapshot', () => {
+    const { asFragment } = render(<MadLibArea />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+
